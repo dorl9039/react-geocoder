c77a5cb3f96586d187b9083067e55eb73ec9dfac
Doris Lin <lmh7026@gmail.com>
2023-06-22 14:55:45 -0400

    implement location search/results handling

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SearchForm = () => {
+const SearchForm = (props) => {
+    const [locationName, setLocationName] = useState('')
 
+    const onFormSubmit = event => {
+        event.preventDefault();
+        props.handleLonLat(locationName);
+        setLocationName('');
+    };
+    const onLocationChange = event => {
+        setLocationName(event.target.value);
+    };
+
+    return (
+        <form onSubmit={onFormSubmit}>
+            <input 
+                placeholder='location'
+                name="locationName" 
+                value={locationName}
+                onChange={onLocationChange}
+                />
+            <input 
+                type="submit" 
+                value="Search Now!" />
+        </form>
+    );
 }
 
 export default SearchForm;
