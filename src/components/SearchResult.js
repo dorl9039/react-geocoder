a5043324b3b7d68d83fdd1b18f6bb52fda7f16e9
Doris Lin <lmh7026@gmail.com>
2023-06-22 14:56:24 -0400

    implement rendering for search results

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const SearchResult = () => {
-
+const SearchResult = (props) => {
+    return (
+        <div className='results-container'>
+            <h2>Results for: {props.cityData}</h2>
+            <ul>
+                <li>Latitude: {props.latData}</li>
+                <li>Longitude: {props.lonData}</li>
+            </ul>
+        </div>
+    );
 }
 
 export default SearchResult;
