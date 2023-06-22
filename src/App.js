4c734bce05830b276a1d501ca204a2c654c89a8c
Doris Lin <lmh7026@gmail.com>
2023-06-22 13:07:59 -0400

    set up components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 
-function App() {
+import SearchForm from './components/SearchForm.js';
+import SearchResult from './components/SearchResult.js';
+import ResultHistory from './components/ResultHistory.js';
 
+function App() {
+  return (
+    <div className="App">
+      <h1>Get Latitude and Longitude</h1>
+      <SearchForm />
+      <SearchResult />
+      <ResultHistory />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ResultRecord.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ResultRecord = () => {
+
+}
+
+export default ResultRecord;
